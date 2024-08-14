512ef3ebdfd83271c3dde4e67a6bcc981cfa6486
Simran Chaurasia <simransia07@gmail.com>
2024-08-14 14:22:13 +0530

    fix cors issue

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(
   cors({
     origin: "*",
-    methods: ["GET", "POST", "OPTIONS"],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
