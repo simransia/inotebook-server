90dea26a8c9f1cb69a33c880beb0f8de1d8ececd
Simran Chaurasia <simransia07@gmail.com>
2024-08-14 14:32:35 +0530

    fix typo

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(
   cors({
     origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Auth - Token", "Authorization"],
+    allowedHeaders: ["Content-Type", "Auth-Token", "Authorization"],
   })
 );
 
